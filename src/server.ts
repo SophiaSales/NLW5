55ec1096ce5f96a734a7eb981776bb993af30d56
SophiaSales <sophiabrenda10@gmail.com>
2021-04-19 20:54:24 -0300

    first day

---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import  express, { request, response } from "express";
+
+const app = express();
+
+app.get("/",(request, response) => {
+    return response.json({
+        message: "Ola NLW5"
+    });
+}); 
+
+app.post("/users", (request, response) => {
+    return response.json({ message: "Usuario cadastrado!"})
+});
+
+app.listen(3333, () => console.log("Servidor is running"));
